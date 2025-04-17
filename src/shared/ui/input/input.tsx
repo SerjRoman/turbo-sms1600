8772deafebb80bc59e3b12686ab8099fc9461ee2
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-04-17 10:14:46 +0200

    Created UI input

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -1,6 +1,24 @@
-import { TextInput } from "react-native";
+import { Text, TextInput, View } from "react-native";
+import { IInputProps } from "./input.types";
+import { styles } from "./input.styles";
+import { ErrorIcon } from "./error-icon";
 
 
-export function Input(props){
-    return <TextInput style={}></TextInput>
+export function Input({ label, errMsg, iconLeft, iconRight, inputStyles, ...props }: IInputProps) {
+    return (
+        <View>
+            {label && <Text style={styles.label}>{label}</Text>}
+            <View style={styles.inputBox}>
+                {iconLeft && <View style={{ marginRight: 2 }}>{iconLeft}</View>}
+                <TextInput style={[inputStyles, styles.input]} {...props}></TextInput>
+                {iconRight && <View style={{ marginLeft: "auto" }}>{iconRight}</View>}
+            </View>
+            {errMsg && (
+                <View style={styles.errorBox}>
+                    <ErrorIcon />
+                    <Text style={styles.errMsg}>{errMsg}</Text>
+                </View>
+            )}
+        </View>
+    )
 }
