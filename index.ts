1a117258205b0380324d5c08400de169cd509040
SerjRoman <sergeyroman26@gmail.com>
2025-05-11 15:49:33 +0100

    comments

--- index.ts
+++ index.ts
@@ -6,3 +6,12 @@ import App from './src/app/App';
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+// Tabs 
+// Expo Router - Tabs (bottom tab)
+// React Native Navigation(RNN) - BottomTabNavigator
+
+
+// Вариант 1 - <View><Header /><Form /></View>
+// Вариант 2 - В Screen проп options, свойство header - передаем компонент Header
+// Вариант 3 - В Screen пропы headerLeft, headerRight - передаем компоненты за куски вашего Header
