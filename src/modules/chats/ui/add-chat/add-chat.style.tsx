3333244ee57ed74651fe3ca2431194b3cc6fd23c
Liudmyla Makhynia <ludmilam20090929@gmail.com>
2025-06-24 19:29:45 +0300

    fix, without avatar

--- src/modules/chats/ui/add-chat/add-chat.style.tsx
+++ src/modules/chats/ui/add-chat/add-chat.style.tsx
@@ -2,21 +2,19 @@ import { StyleSheet } from "react-native";
 import { COLORS } from "../../../../shared/constants";
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    backgroundColor: COLORS.white,
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderBottomColor: COLORS.grey
-  },
-  title: {
-    fontSize: 32,
-  },
-  contactList: {
-    
-  }
+	container: {
+		flex: 1,
+		paddingTop: 30,
+        backgroundColor:COLORS.white,
+	},
+
+	item: {
+		backgroundColor: COLORS.white,
+		padding: 20,
+		borderBottomColor: COLORS.grey,
+        borderBottomWidth:2
+	},
+	title: {
+		fontSize: 32,
+	},
 });
--- src/modules/contacts/ui/list/list.tsx
+++ src/modules/contacts/ui/list/list.tsx
@@ -1,13 +1,36 @@
-import { FlatList, FlatListProps } from "react-native";
+import { FlatList, FlatListProps, View, Text } from "react-native";
 import { styles } from "./list.styles";
-
-export function ContactList<T>(props: FlatListProps<T>) {
+import { useGetContacts } from "../../hooks/use-get-contacts";
+import { useUserContext } from "../../../auth/context";
+import { Contact } from "../../types/contact";
+// props: FlatListProps<T>
+export function ContactList() {
 	// return <FlatList {...props} />;
-	return (
-		<FlatList
-			style={styles.contactList}
-			data={undefined}
-			renderItem={undefined}
-		></FlatList>
-	);
+    const { contactsList } = useGetContacts();
+    const { user } = useUserContext()
+    console.log(contactsList);
+
+    const Item = ({ localName, avatar }: Contact) => (
+        <View style={styles.item}>
+            <Text style={styles.title}>{localName}</Text>
+        </View>
+    );
+    return (
+        <FlatList
+            style={styles.contactList}
+            data={contactsList}
+            renderItem={({ item }) => (
+                <Item
+                    key={item.id}
+                    localName={item.localName}
+                    avatar={""}
+                    id={0}
+                    addedAt={new Date}
+                    contactUserId={item.id}
+                    ownerId={user? user.id : 0}
+                />
+            )}
+            keyExtractor={(item) => item.id.toString()}
+        ></FlatList>
+    );
 }
